04e8f9415a80a0c0fe71a9d5c104da8fd3ad3a5c
Trevor Tuchten <tntdirtracer@gmail.com>
2016-10-09 06:59:45 -0700

    style commit

--- src/app/courses/courses.component.spec.ts
+++ src/app/courses/courses.component.spec.ts
@@ -4,8 +4,4 @@ import { TestBed, async } from '@angular/core/testing';
 import { CoursesComponent } from './courses.component';
 
 describe('Component: Courses', () => {
-  it('should create an instance', () => {
-    let component = new CoursesComponent();
-    expect(component).toBeTruthy();
-  });
 });
